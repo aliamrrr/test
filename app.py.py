92744c5778c3264eae412d683c9f597b28b7f42f
aliamrrr <144249890+aliamrrr@users.noreply.github.com>
2025-01-17 12:58:38 +0100

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Hello, World! with Streamlit")
+
+st.write("Welcome to your first Streamlit app 🚀")
+
+if st.button("Click me"):
+    st.success("You clicked the button! 🎉")
